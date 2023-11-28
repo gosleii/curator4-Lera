a4b38683e7e8c2ee2eb143048856cacf6ea77fa8
Pavel Sura <sleii@inbox.ru>
2023-11-29 01:38:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+import telebot
+from telebot import custom_filters
+from telebot import StateMemoryStorage
+from telebot.handler_backends import StatesGroup, State
+
+state_storage = StateMemoryStorage()
+# Вставить свой токет или оставить как есть, тогда мы создадим его сами
+bot = telebot.TeleBot("6383786880:AAEtvsu3PfRzwwdIf2OkBnOJF6sh14oO_J4",
+                      state_storage=state_storage, parse_mode='Markdown')
+
+
+class PollState(StatesGroup):
+    name = State()
+    age = State()
+
+
+class HelpState(StatesGroup):
+    wait_text = State()
+
+
+text_poll = "Опрос"  # Можно менять текст
+text_button_1 = "Музыка"  # Можно менять текст
+text_button_2 = "Медитация"  # Можно менять текст
+text_button_3 = "Картинки"  # Можно менять текст
+
+menu_keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_poll,
+    )
+)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_1,
+    )
+)
+
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_2,
+    ),
+    telebot.types.KeyboardButton(
+        text_button_3,
+    )
+)
+
+
+@bot.message_handler(state="*", commands=['start'])
+def start_ex(message):
+    bot.send_message(
+        message.chat.id,
+        'Привет! Это бот для *медитаций*. Чего бы хотелось сейчас?',  # Можно менять текст
+        reply_markup=menu_keyboard)
+
+
+@bot.message_handler(func=lambda message: text_poll == message.text)
+def first(message):
+    bot.send_message(message.chat.id, 'Супер! _Знакомы ли вы с медитациями_?')  # Можно менять текст
+    bot.set_state(message.from_user.id, PollState.name, message.chat.id)
+
+
+@bot.message_handler(state=PollState.name)
+def name(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['name'] = message.text
+    bot.send_message(message.chat.id,
+                     'Если хочется узнать о них больше, вот [статья](https://dzen.ru/a/ZMzBPXRYuS15awOc). *Как вам?*')  # Можно менять текст
+    bot.set_state(message.from_user.id, PollState.age, message.chat.id)
+
+
+@bot.message_handler(state=PollState.age)
+def age(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['age'] = message.text
+    bot.send_message(message.chat.id, 'Спасибо за проявленный интерес!',
+                     reply_markup=menu_keyboard)  # Можно менять текст
+    bot.delete_state(message.from_user.id, message.chat.id)
+
+
+@bot.message_handler(func=lambda message: text_button_1 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "(https://www.youtube.com/watch?v=kndqIj8Qgok)",
+                     reply_markup=menu_keyboard)  # Можно менять текст
+
+
+@bot.message_handler(func=lambda message: text_button_2 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "(https://www.youtube.com/watch?v=edR8bnvF_wk)",
+                     reply_markup=menu_keyboard)  # Можно менять текст
+
+
+@bot.message_handler(func=lambda message: text_button_3 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "(https://ru.freepik.com/photos/meditation)",
+                     reply_markup=menu_keyboard)  # Можно менять текст
+
+
+bot.add_custom_filter(custom_filters.StateFilter(bot))
+bot.add_custom_filter(custom_filters.TextMatchFilter())
+
+bot.infinity_polling()
